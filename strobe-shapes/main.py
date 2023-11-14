7b0189910794aa9f0afe020019ef53ccbb406ada
tomas <tomasbelaxinen@gmail.com>
2023-11-14 12:27:05 +0100

    -Make random size color arcs

---
+++ strobe-shapes/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import random
+
+
+canvas = Canvas( width=1000, height=1000)
+canvas.pack()
+colors = ["green","blue","pink","cyan","black","white","magenta","orange"]
+while True:
+    canvas.create_arc(random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),fill=random.choice(colors),start=random.randint(1,360))
+    canvas.update()
+
+canvas.mainloop()
+
