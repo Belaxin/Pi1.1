5d6653ee609fe8e306a0721b8a2a78895db780dc
tomas <tomasbelaxinen@gmail.com>
2023-11-28 22:44:34 +0100

    -Add Sutek-Meno2.py

--- strobe-shapes/strobes.py
+++ strobe-shapes/strobes.py
@@ -6,7 +6,7 @@ canvas = Canvas( width=1000, height=1000)
 canvas.pack()
 colors = ["green","blue","pink","cyan","black","white","magenta","orange"]
 while True:
-    canvas.create_arc(random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),fill=random.choice(colors),start=random.randint(1,360),extent=random.randint(1,360))
+    canvas.create_arc(random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),fill=color,start=random.randint(1,360),extent=random.randint(1,360))
     canvas.update()
 
 canvas.mainloop()
