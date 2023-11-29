c8427f7e83356677101e3bef22d01792b32dda76
Tomas Matis <tomasbelaxinen@gmail.com>
2023-11-29 09:44:40 +0100

    Delete empty folder+ delete redundant Sutek-Meno2.py

--- strobe-shapes/strobes.py
+++ strobe-shapes/strobes.py
@@ -6,6 +6,7 @@ canvas = Canvas( width=1000, height=1000)
 canvas.pack()
 colors = ["green","blue","pink","cyan","black","white","magenta","orange"]
 while True:
+    color = random.choice(colors)
     canvas.create_arc(random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),random.randint(0,1000),fill=color,start=random.randint(1,360),extent=random.randint(1,360))
     canvas.update()
 
