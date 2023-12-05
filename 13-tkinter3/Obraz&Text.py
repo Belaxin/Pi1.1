17fd87cdfd81b9b71c18a587c5baffd22e18f386
tomas <tomasbelaxinen@gmail.com>
2023-12-05 22:07:36 +0100

    add ptarmigan fall

--- 13-tkinter3/Obraz&Text.py
+++ 13-tkinter3/Obraz&Text.py
@@ -1,8 +1,19 @@
+import time
 from tkinter import *
 
-canvas = Canvas(height=400,width=800)
+canvas = Canvas(height=800,width=800)
 canvas.pack()
+x = 400
+y = 200
+def fall(y):
+    return y+100
+while True :
+    time.sleep(0.5)
+    ptarmigan = PhotoImage(file='./ptarmigan.gif')
+    photo = canvas.create_image(x,y, image = ptarmigan)
+    y = fall(y)
+    print(y)
+    if y>800:
+        y = 0
+    canvas.update()
 
-ptarmigan = PhotoImage(file='./ptarmigan.gif')
-canvas.create_image(400,200, image = ptarmigan)
-canvas.mainloop()
