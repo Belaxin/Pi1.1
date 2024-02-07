8e6d0ede1dccccf9ad2634315efde2b6f8d31644
Tomas Matis <tomasbelaxinen@gmail.com>
2024-02-07 08:58:53 +0100

    -add color

--- 16-fukncie2/stvorce.py
+++ 16-fukncie2/stvorce.py
@@ -3,12 +3,24 @@ import tkinter as tk
 canvas = tk.Canvas(height=540, width=960)
 canvas.pack()
 
+def rgb(r, g, b):
+    return f"#{r:02x}{g:02x}{b:02x}"
 
-def stvorce(x, y, pocet, dlzka):
+def stvorce(x, y, pocet, dlzka, r=255 ,g=255 ,b=255):
+    krok = 5
     for i in range(pocet):
-        canvas.create_rectangle(x - dlzka // 2, y - dlzka // 2, x + dlzka // 2, y + dlzka // 2)
+        canvas.create_rectangle(x - dlzka // 2, y - dlzka // 2, x + dlzka // 2, y + dlzka // 2, fill=rgb(r, g, b))
         x += dlzka
-
-
-stvorce(20, 20, 10, 30)
+        if r>=krok:
+            r -= krok
+        if g >= krok :
+            g -= krok
+        if b >= krok :
+            b -= krok
+y = 20
+for i in range (5):
+    stvorce(20,y,47,20,255,0,0)
+    stvorce(20,y +20,47,20,0,255,0)
+    stvorce(20,y +40,47,20,0,0,255)
+    y +=60
 canvas.mainloop()
