aa8138ebeff2acecce6b0a3027ce4b275a8172d2
Tomas Matis <tomasbelaxinen@gmail.com>
2024-03-06 11:12:29 +0100

    -add 18-turtle2

--- 18-turtle2/5.py
+++ 18-turtle2/5.py
@@ -9,11 +9,11 @@ for i in range(pocet):
     t.setposition(i* 100, 0)
     t.pendown()
     turtles.append(t)
+for t in turtles:
 
-
-# for i in range(4):
-#     for t in turtles:
-#         t.forward(50)
-#         t.left(90)
+    for i in range(4):
+        for j in range(10):
+            t.forward(50)
+            t.left(90)
 
 turtle.mainloop()
