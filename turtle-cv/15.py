dbf6c64468ff3f556eff48dbad36ef241a7fec11
Tomas Matis <tomasbelaxinen@gmail.com>
2024-03-27 11:03:00 +0100

    -comments 15.py

--- turtle-cv/15.py
+++ turtle-cv/15.py
@@ -12,22 +12,26 @@ def square(dlzka):
 
 
 def Pytagoras(Prepona, uhol):
+    # prepona
     t.fillcolor("Blue")
     t.begin_fill()
     square(Prepona)
     t.fd(Prepona)
     t.end_fill()
+    # odvesna 1
     t.fillcolor("Red")
     t.begin_fill()
     t.right(180 - uhol)
     square(Prepona * (math.cos(math.radians(uhol))))
     t.fd(Prepona * (math.cos(math.radians(uhol))))
     t.end_fill()
+    # odvesna 2
     t.fillcolor("Yellow")
     t.begin_fill()
     t.right(90)
     square(Prepona * (math.sin(math.radians(uhol))))
     t.end_fill()
+    # vypocty
     sucet = (Prepona * (math.sin(math.radians(uhol)))) ** 2 + (Prepona * (math.cos(math.radians(uhol)))) ** 2
     print("Stvorec nad preponou: " + str(Prepona ** 2))
     print("Stvorec nad 1. odvesnou: " + str((Prepona * (math.sin(math.radians(uhol)))) ** 2))
